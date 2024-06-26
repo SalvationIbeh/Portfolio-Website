dd9567fe957d64e998c48bc813ab34fd313818dc
salvation <salvationibeh@gmail.com>
2024-06-26 15:41:05 +0100

    Converted eslintrc.json to mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import _import from 'eslint-plugin-import';
+import node from 'eslint-plugin-node';
+import promise from 'eslint-plugin-promise';
+import standard from 'eslint-plugin-standard';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...fixupConfigRules(compat.extends(
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:node/recommended',
+    'plugin:promise/recommended',
+    'standard'
+  )),
+  {
+    plugins: {
+      import: fixupPluginRules(_import),
+      node: fixupPluginRules(node),
+      promise: fixupPluginRules(promise),
+      standard
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module'
+    },
+
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['warn'],
+      'no-undef': ['error'],
+      'no-console': 'off'
+    }
+  }
+];
